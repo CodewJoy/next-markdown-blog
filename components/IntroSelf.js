d298f7c7d3ad038f6f02730f081a1626c80be26e
Joy Hung <yichen.hung6699@gmail.com>
2022-10-09 21:08:33 +0800

    modify intro self avatar src

--- components/IntroSelf.js
+++ components/IntroSelf.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function IntroSelf() {
   return (
     <asid className='intro-self'>
-      <Image src='/avatar.jpg' alt='Joy Hung' width='56' height='56' layout='fixed' style={{ borderRadius: '50%' }}/>
+      <Image src='/images/avatar.jpg' alt='Joy Hung' width='56' height='56' layout='fixed' style={{ borderRadius: '50%' }}/>
       <p className='intro-self-text'>Personal blog by&nbsp;
         <Link href='https://github.com/CodewJoy' passHref>
           <a className='text-color uderline'>Joy Hung</a> 
